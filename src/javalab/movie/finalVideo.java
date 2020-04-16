2e887d61559f128b2a1db256083f3a911455ecf4
hadoop <hadoop@Dhirajs-MacBook-Pro.local>
2020-04-16 22:14:49 +0530

    second commit

---
+++ src/javalab/movie/finalVideo.java
@@ -0,0 +1,48 @@
+package javalab.movie;
+
+import java.util.*;
+
+public class finalVideo {
+
+	public static void main(String[] args) {
+		
+		videoProcess mm = new videoProcess();
+		while(true)
+		{
+				System.out.println("VIDEO LIBRARY CENTER");
+				int n;
+				Scanner in = new Scanner(System.in);
+				System.out.println("1.ADD MOVIES");
+				System.out.println("2.DISPLAY MOVIES");
+				System.out.println("3.RENT OUT");
+				System.out.println("4.COLLECT BACK ");
+	
+				System.out.println("PLEASE ENTER YOUR OPTION");
+				n = in.nextInt();
+				switch(n)
+				{
+					case 1:mm.AddMovies();
+					break;
+					case 2:mm.DisplayAll();
+					break;
+					case 3:System.out.print("Enter the movie you want to rent.");
+					in.nextLine();
+					if(mm.RentOut(in.nextLine()))
+					{
+						System.out.println("Rent out successfull");
+					}
+					else
+					{
+						System.out.println("Sorry!! Not Available");
+					}
+					break;
+					case 4:System.out.println("Enter the name and the ratings of the movie"); 
+					in.nextLine();
+					mm.CollectIn(in.nextLine(),in.nextDouble());  
+					break;
+				}
+		}
+
+	}
+
+}
