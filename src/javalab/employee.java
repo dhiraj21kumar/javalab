8521625d2be79bd70206bb0b03f5bee40e10a59f
Dhiraj Kumar <37096978+dhiraj21kumar@users.noreply.github.com>
2020-04-18 11:18:21 +0530

    Update employee.java

--- src/javalab/employee.java
+++ src/javalab/employee.java
@@ -1,3 +1,5 @@
+//Part B 1. Develop a small java application, which accepts employee id from the command prompt and displays the details using arrays.
+
 package javalab;
 
 import java.util.*;
@@ -7,16 +9,16 @@ public class employee {
 	public static void main(String[] args) {
 		
 		System.out.println("Enter Valid Employee ID : \n");
-		int[] EmpId={1001,1002,1003,1004,1005,1006,1007};
-		String[] EmpName={"Abc","Opqr","Ghi","Wxyz","Jklmn","Stuv","Def"};
-		String[] JoinDate={"01/04/2009","23/08/2012","12/11/2008","29/01/2013","16/07/2005","01/01/2000","12/06/2006"};
+		int[] EmpId={1001,1002,1003,1004,1005,1006,1007};     //Predefining the Employee Id.
+		String[] EmpName={"Abc","Opqr","Ghi","Wxyz","Jklmn","Stuv","Def"};  //Predefining the employees name.
+		String[] JoinDate={"01/04/2009","23/08/2012","12/11/2008","29/01/2013","16/07/2005","01/01/2000","12/06/2006"};  //Predefining the employees DOJ.
 		char[] DesigCode={'e','c','k','r','m','e','c'};
-		String[] Department={"R&D","PM","Acct","Front Desk","Engg","Manufacturing","PM"};
-		double[] Basic={20000,30000,10000,12000,50000,23000,29000};
+		String[] Department={"R&D","PM","Acct","Front Desk","Engg","Manufacturing","PM"};  //Predefining the employees dept.
+		double[] Basic={20000,30000,10000,12000,50000,23000,29000}; 
 		double[] HRA={8000,12000,8000,6000,20000,9000,12000};
 		double[] IT={3000,9000,1000,2000,20000,4400,10000};
 		char[] DesignationCode={'e','c','k','r','m'};
-		String[] Designation={"Engineer","Consultant","Clerk","Receptionist","Manager"};
+		String[] Designation={"Engineer","Consultant","Clerk","Receptionist","Manager"};  //Predefining the employees designation.
 		double[] DA={20000,32000,12000,15000,40000};
 		int flag=0;
 		int id=Integer.parseInt(args[0]);
