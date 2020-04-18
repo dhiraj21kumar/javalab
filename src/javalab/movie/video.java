974d8fd80370bf7d84661c02db06fd93e75e51b3
Dhiraj Kumar <37096978+dhiraj21kumar@users.noreply.github.com>
2020-04-18 19:30:45 +0530

    Update video.java

--- src/javalab/movie/video.java
+++ src/javalab/movie/video.java
@@ -7,34 +7,34 @@ public class video {
     boolean status;
     double rating;
     
-	 public video(String mName, boolean status, double rating) 
+	 public video(String mName, boolean status, double rating)   	//Take the movie details from the user.
 	 {
 		super();
 		this.mName = mName;
 		this.status = status;
 		this.rating = rating;	
 	 }
-	public String getmName()
+	public String getmName()			//Used to get the movie name the user has given.
 	{
 		return mName;
 	}
-	public void setmName(String mName) 
+	public void setmName(String mName) 		//Used to set the name of new movie that the user defines.
 	{
 		this.mName = mName;
 	}
-	public boolean isStatus()
+	public boolean isStatus()			//Get the status if the movie.
 	{
 		return status;
 	}
-	public void setStatus(boolean status) 
+	public void setStatus(boolean status) 		//Setting the status of the o=movie while entering about some new movie.
 	{
 		this.status = status;
 	}
-	public double getRating() 
+	public double getRating() 			//Get the rating of the movie that the user has defined.
 	{
 		return rating;
 	}
-	public double setRating(double rating) 
+	public double setRating(double rating) 		//Setting the rating of some new movie.
 	{
 		return this.rating = rating;
 	} 
