5a6a478683084b29d3217f125e6038455922d084
Dhiraj Kumar <37096978+dhiraj21kumar@users.noreply.github.com>
2020-04-18 11:09:43 +0530

    Update password.java

--- src/javalab/password.java
+++ src/javalab/password.java
@@ -1,3 +1,5 @@
+#Part A 6.Design an algorithm which accepts a Room Number, Mobile Number and Name of the Customer and generate a 6 Character Unique Password.
+
 package javalab;
 
 import java.util.*;
